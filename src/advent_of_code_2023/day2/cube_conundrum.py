43fbe4e47846a190d57851065917a31316554ba2
Antonino Cangialosi <ninocangialosi@yahoo.it>
2023-12-02 12:14:31 +0100

    Tweak project confs

--- src/advent_of_code_2023/day2/cube_conundrum.py
+++ src/advent_of_code_2023/day2/cube_conundrum.py
@@ -4,14 +4,17 @@ from typing import List
 from .bag import Bag
 
 
-def sum_ids_of_possible_games_with_constraint(games: List[str], constraint: Bag=Bag(12, 13, 14)) -> int:
-    """ Determine which games are possible, given a constraint. Then, sum their ids.
+def sum_ids_of_possible_games_with_constraint(
+    games: list[str],
+    constraint: Bag = Bag(12, 13, 14),
+) -> int:
+    """Determine which games are possible, given a constraint. Then, sum their ids.
     The default constraint is (12,13,14)"""
 
 
 if __name__ == "__main__":
     file_path = Path(__file__).parent / "input.txt"
-    with open(file_path, 'r') as file:
+    with open(file_path) as file:
         lines = file.readlines()
     result = sum_ids_of_possible_games_with_constraint(lines)
     print(result)
