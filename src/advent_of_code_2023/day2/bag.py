c3c117137215e919e6383327c3e36f8aa39fe53a
Antonino Cangialosi <ninocangialosi@yahoo.it>
2023-12-02 12:14:57 +0100

    Add comparator

--- src/advent_of_code_2023/day2/bag.py
+++ src/advent_of_code_2023/day2/bag.py
@@ -1,11 +1,20 @@
 """
 This module contains the modelling of the bag and the functions acting on it.
 """
-from dataclasses import dataclass
+from __future__ import annotations
+
+from dataclasses import dataclass, fields
+
 
 @dataclass
 class Bag:
-    red: int
-    blue: int
-    green: int
+    red: int = 0
+    blue: int = 0
+    green: int = 0
 
+    def satisfy_constraint(self, constraint: Bag) -> bool:
+        all_attrs = fields(self)
+        return all(
+            getattr(self, field.name) < getattr(constraint, field.name)
+            for field in all_attrs
+        )
