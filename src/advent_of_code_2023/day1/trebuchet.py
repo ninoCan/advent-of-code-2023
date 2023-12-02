9f6a6cb5a8928fac5aabd2924ca0788f80e848c2
Antonino Cangialosi <ninocangialosi@yahoo.it>
2023-12-02 09:51:33 +0100

    Add implentation of problem 2

--- src/advent_of_code_2023/day1/trebuchet.py
+++ src/advent_of_code_2023/day1/trebuchet.py
@@ -1,12 +1,13 @@
 from pathlib import Path
-from typing import List
+from typing import List, Union
 import re
 
 
 def sum_all_digits_in_file(file_content: List[str]) -> int:
     """ Cycle lines of input file, extract first and last digit to compose the
     number, and sum them all to get the total"""
-    return sum([extract_first_and_last_digits(line) for line in file_content])
+    numbers_to_sum = [extract_first_and_last_digits_with_words(line) for line in file_content]
+    return sum(numbers_to_sum)
 
 
 def extract_first_and_last_digits(line_content: str) -> int:
@@ -15,6 +16,34 @@ def extract_first_and_last_digits(line_content: str) -> int:
         return 0
     first, last = digits[0], digits[-1]
     return int(f"{first}{last}")
+
+
+def extract_first_and_last_digits_with_words(line_content: str) -> int:
+    digits_pattern = r"\d"
+    word_pattern = r"one|two|three|four|five|six|seven|eight|nine"
+    pattern = re.compile(f"{digits_pattern}|{word_pattern}")
+    digits_and_words = pattern.findall(line_content)
+    if not digits_and_words:
+        return 0
+    digits = [word_to_digit(el) for el in digits_and_words]
+    return int(f"{digits[0]}{digits[-1]}")
+
+
+def word_to_digit(el: str) -> int:
+    if len(el) == 1:
+        return int(el)
+    word_to_digit_mapping = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        }
+    return word_to_digit_mapping.get(el)
 
 
 if __name__ == "__main__":
--- tests/day1/test_trebuchet.py
+++ tests/day1/test_trebuchet.py
@@ -5,14 +5,15 @@ from src.advent_of_code_2023.day1.trebuchet import sum_all_digits_in_file
 
 def test_sum_all_digits_in_file():
     input_prompt = """
-1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet"""
-
-    actual: int = sum_all_digits_in_file(input_prompt.split('\n'))
-    expected: int = 142
-    assert actual == expected
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"""
     
-
-
+    actual: int = sum_all_digits_in_file(input_prompt.split('\n'))
+    expected: int = 281
+    assert actual == expected
