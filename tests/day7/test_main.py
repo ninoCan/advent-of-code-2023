9362ea30be9737a08157bb6e37a3049566428307
Antonino Cangialosi <ninocangialosi@yahoo.it>
2023-12-10 00:55:23 +0100

    Update test suite

--- tests/day7/test_main.py
+++ tests/day7/test_main.py
@@ -9,6 +9,7 @@ import pytest
 from src.advent_of_code_2023.day7.main import (
     main,
     part_two_main,
+    order_by_rank,
 )
 
 
@@ -30,3 +31,15 @@ def test_part_two_main(provide_test_lines: List[str]) -> None:
     expected = "placeholder"
     actual = part_two_main(provide_test_lines)
     assert actual == expected
+
+
+@pytest.mark.parametrize(
+    'stub_cards_bids, expected',
+    [
+        (("AAAAA 3", "TK987 1", "TTKK9 2"), ('AAAAA 3', 'TTKK9 2', 'TK987 1')),
+        (("QQQ89 1", "TTTT2 3", "KKK22 2"), ("TTTT2 3", "KKK22 2", "QQQ89 1")),
+    ],
+)
+def test_order_by_rank(stub_cards_bids, expected) -> None:
+    actual = order_by_rank(stub_cards_bids)
+    assert expected == actual
