15cc1589dc63aecba5505b92eaa0b9de0507c4f2
Antonino Cangialosi <ninocangialosi@yahoo.it>
2023-12-11 21:48:53 +0100

    Design main function logic

--- src/advent_of_code_2023/day11/main.py
+++ src/advent_of_code_2023/day11/main.py
@@ -1,9 +1,20 @@
+import itertools
 from pathlib import Path
 from typing import List
 
+from advent_of_code_2023.day11.Universe import Universe
+
+
+def l1_distance(pair):
+    pass
+
 
 def main(lines: List[str]) -> int:
-    pass
+    past_universe = Universe(lines)
+    present_universe = past_universe.expand()
+    galaxies = present_universe.galaxies
+    paired_galaxies = itertools.pairwise(galaxies)
+    return sum(l1_distance(galaxy_pair) for galaxy_pair in paired_galaxies)
 
 
 def part_two_main(lines: List[str]) -> int:
