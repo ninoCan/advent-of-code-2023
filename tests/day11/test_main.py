40cae9c937e463169b46e814a9af2e7c3ce76a88
Antonino Cangialosi <ninocangialosi@yahoo.it>
2023-12-11 13:42:14 +0100

    test: initialize and extend part 1 test suite

--- tests/day11/test_main.py
+++ tests/day11/test_main.py
@@ -1,5 +1,3 @@
-""" This is a template file do not use it directly.
-"""
 from inspect import getsourcefile
 from pathlib import Path
 from typing import List
@@ -9,24 +7,38 @@ import pytest
 from src.advent_of_code_2023.day11.main import (
     main,
     part_two_main,
+    expand_universe,
 )
 
 
 @pytest.fixture
-def provide_test_lines() -> List[str]:
+def lines_stub() -> List[str]:
     source_path = Path(getsourcefile(main)).resolve().parent / 'README.md'
     with source_path.open("r") as file:
-        example_slice = slice("n - 1", "m")
+        example_slice = slice(18, 28)
         return [line.strip() for line in file.readlines()[example_slice]]
 
 
-def test_main(provide_test_lines: List[str]) -> None:
-    expected = "placeholder"
-    actual = main(provide_test_lines)
+@pytest.fixture
+def expected_universe() -> List[str]:
+    source_path = Path(getsourcefile(main)).resolve().parent / 'README.md'
+    with source_path.open("r") as file:
+        example_slice = slice(56, 68)
+        return [line.strip() for line in file.readlines()[example_slice]]
+
+
+def test_main(lines_stub) -> None:
+    expected = 374
+    actual = main(lines_stub)
     assert actual == expected
 
 
-def test_part_two_main(provide_test_lines: List[str]) -> None:
+def test_expand_universe(lines_stub, expected_universe) -> None:
+    actual = expand_universe(lines_stub)
+    assert actual == expected_universe
+
+
+def test_part_two_main(lines_stub) -> None:
     expected = "placeholder"
-    actual = part_two_main(provide_test_lines)
+    actual = part_two_main(lines_stub)
     assert actual == expected
